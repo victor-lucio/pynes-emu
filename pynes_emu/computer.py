a94dcf12ab865703593d91fb370efa9de587199d
Victor Lucio <victor.lucio10@gmail.com>
2025-01-26 02:09:28 -0300

    added a program and a way to load it

--- pynes_emu/computer.py
+++ pynes_emu/computer.py
@@ -3,22 +3,33 @@ from pynes_emu.memory import Memory
 
 
 class Computer:
-    def __init__(self):
+    def __init__(self, program):
         self.memory = Memory()
-        self._copy_program_to_memory()
+        self._copy_program_to_memory(program)
 
         self.cpu = Cpu(memory=self.memory)
         self.cpu.reset()
         print(self.cpu)
 
-    def _copy_program_to_memory(self):
-        # set the program counter to the start of the program
-        self.memory[PC_START_INDIRECT_LOCATION, 2] = 0x00FF
+    def _copy_program_to_memory(self, program):
 
-        program = [0xA9, 0xC0, 0xAA, 0xE8]
+        # read program from programs folder
+        if isinstance(program, str):
+            with open(f"programs/{program}", "r") as f:
+                program_raw = list(f.readlines())
+
+        # remove comments, empty spaces and empty lines
+        program_str = [line.strip() for line in program_raw if line and not line.startswith("//")]
+
+        # convert hex strings to integers
+        program_hex = [int(line, 16) for line in program_str]
+
+        # set the program counter to the start of the program
+        start_address = 0x8000
+        self.memory[PC_START_INDIRECT_LOCATION, 2] = start_address
 
         # load program
-        self.memory[0x00FF : 0x00FF + len(program)] = program
+        self.memory[start_address : start_address + len(program_hex)] = program_hex
         return
 
     def run(self):
